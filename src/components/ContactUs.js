7c982fa14cb3eadcf1af47fc577934aba96d9c6f
Ferdz <ferheredia99@gmail.com>
2021-09-15 16:53:32 -0600

    styling contact Us pagee

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,68 +1,115 @@
-import React, {useState} from 'react';
-import 'font-awesome/css/font-awesome.min.css';
-import './ContactUs.css'
-import { Card } from "primereact/card";
-import emailjs from 'emailjs-com';
+import React, { useState, useRef } from "react";
+import "font-awesome/css/font-awesome.min.css";
+import { Toast } from "primereact/toast";
+import { Button } from "primereact/button";
+import "./ToastDemo.css";
 
-const Result = () =>{
-  return (<p> Your Message has been successfully sent. You will be contacted soon</p>
-    );
-  
-}
+import "./ContactUs.css";
+import { Card } from "primereact/card";
+import emailjs from "emailjs-com";
+
+const Result = () => {
+  return (
+    <p> Your Message has been successfully sent. You will be contacted soon</p>
+  );
+};
 
 const ContactUs = () => {
+
+  const showSuccess = () => {
+    toast.current.show({
+      severity: "success",
+      summary: "Success Message",
+      detail: "Message Content",
+      life: 3000,
+    });
+  };
+  const toast = useRef(null);
   const [result, showResult] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_lxa9p3s', 'template_g3tkoeb', e.target, 'user_qcjsnmbpDqqXLxyJLXcG6')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_lxa9p3s",
+        "template_g3tkoeb",
+        e.target,
+        "user_qcjsnmbpDqqXLxyJLXcG6"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      e.target.reset();
-      showResult(true);
-  }
+        }
+      );
+    e.target.reset();
+    showResult(true);
+  };
 
   return (
-    <Card className="p-col-6" style={{ margin: "auto", marginTop: "1rem"}}>
-    <div>
-      <div className="contactme" id="contact">
-      <div className="contactOverlay">
-        <div className="container">
-          <div className="form">
-            <form action="" onSubmit={sendEmail}>
-              <div className="formWord">
-                <h2>Contact Us!</h2>
-                <span>Full Name</span>
-                <br />
-                <input className="input100" type="text" name="fullName" required />
-                <br />
-                <span>Phone Number</span>
-                <br />
-                <input className="input100" type="text" name="phone" required />
-                <br />
-                <span>Enter Email</span>
-                <br />
-                <input className="input100" type="text" name="email" required />
-                <br />
-                <div className="formWord">
-                <span>Message</span>
-                <br />
-                <textarea name="message" required></textarea>
-                <br />
-                <div className="submitButton"><button >SUBMIT</button></div>
-                <div className="row">{result ? <Result/>: null}</div>
+    <Card className="p-col-6" style={{ margin: "auto", marginTop: "1rem" }}>
+      <div>
+        <Toast ref={toast} />
+        <h5>Severities</h5>
+        <Button
+          label="Success"
+          className="p-button-success"
+          onClick={showSuccess}
+        />
+        <div className="contactme" id="contact">
+          <div className="contactOverlay">
+            <div className="container">
+              <div className="form">
+                <form action="" onSubmit={sendEmail}>
+                  <div className="formWord">
+                    <h2>Contact Us!</h2>
+                    <span>Full Name</span>
+                    <br />
+                    <input
+                      className="input100"
+                      type="text"
+                      name="fullName"
+                      required
+                    />
+                    <br />
+                    <span>Phone Number</span>
+                    <br />
+                    <input
+                      className="input100"
+                      type="text"
+                      name="phone"
+                      required
+                    />
+                    <br />
+                    <span>Enter Email</span>
+                    <br />
+                    <input
+                      className="input100"
+                      type="text"
+                      name="email"
+                      required
+                    />
+                    <br />
+                    <div className="formWord">
+                      <span>Message</span>
+                      <br />
+                      <textarea name="message" required></textarea>
+                      <br />
+                      <div className="submitButton">
+                        <button>SUBMIT</button>
+                      </div>
+                      <div className="row">{result ? <showSuccess /> : null}</div>
+                    </div>
+                  </div>
+                </form>
               </div>
-              </div>
-             
-            </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     </Card>
   );
 };
